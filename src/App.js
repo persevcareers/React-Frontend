ba448220504562bb134e938ea33cafad6122c1de
persevcareers <148257733+persevcareers@users.noreply.github.com>
2024-03-21 15:35:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Tasks {
         return (
             <div className="app">
                 <header className="app-header">
-                    <h1>Perseverance Checklist</h1>
+                    <h1>Perseverance Student Checklist</h1>
                 </header>
                 <div className="main-content">
                     <Paper elevation={3} className="todo-container">
